88422e14cce9adde3173e67212ee7973368ad58f
Dirk Lemstra <dirk@lemstra.org>
2022-02-25 10:49:15 +0100

    Added ProjectPlanning.

--- app/Output.cs
+++ app/Output.cs
@@ -1,3 +1,10 @@
 ﻿internal class Output
 {
+    private readonly List<ProjectPlanning> _planning = new();
+
+    public IReadOnlyList<ProjectPlanning> Planning
+        => _planning;
+
+    public void  AddProjectPlanning(ProjectPlanning projectPlanning)
+        => _planning.Add(projectPlanning);
 }
---
+++ app/ProjectPlanning.cs
@@ -0,0 +1,17 @@
+﻿internal  class ProjectPlanning
+{
+    private readonly List<Contributor> _contributors = new();
+
+    public ProjectPlanning(Project project)
+    {
+        Project = project;
+    }
+
+    public Project Project { get; }
+
+    public IReadOnlyList<Contributor> Contributors
+        => _contributors;
+
+    public void AddContributor(Contributor contributor)
+        => _contributors.Add(contributor);
+}
