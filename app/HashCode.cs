453e95d0d41b4f5557bf58e37fed7c3e24c768b8
RonaldKruizinga <ronmatk@gmail.com>
2022-02-25 11:21:47 +0100

    Add mockmethods for implementation

--- app/HashCode.cs
+++ app/HashCode.cs
@@ -5,7 +5,7 @@
         //todo: make mutable copies
         List<Project> projects = input.Projects.ToList();
         List<Contributor> contributors = input.Contributors.ToList();
-        List<Project> completedProjects = new();
+        Output output = new();
         while (projects.Any())
         {
             List<Project> feasibleProjects = CalculateFeasible(projects, contributors);
@@ -14,23 +14,43 @@
                 break;
             }
             Project bestProject = PickBestProject(feasibleProjects);
+
+            ProjectPlanning projectPlanning = new(bestProject);
+            //todo: assign correct contributors, mostly focussing on skill improvement if possible
+            projectPlanning.AddContributor(contributors[0]);
+
             projects.Remove(bestProject);
-            UpdateContributors(contributors);
-            completedProjects.Add(bestProject);
+            UpdateContributorSkillLevel(contributors);
+            output.AddProjectPlanning(projectPlanning);
         }
 
-        return new Output();
+        return output;
     }
 
+    /// <summary>
+    /// Find all projects where required skills are available.
+    /// future: filter out projects with score 0
+    /// futurefuture: consider improving skills through 0 score projects
+    /// </summary>
     private static List<Project> CalculateFeasible(List<Project> projects, List<Contributor> contributors)
     {
-        //todo: projectassignees
         projects = HaveRequiredSkills(projects, contributors);
         return projects;
     }
 
+    /// <summary>
+    /// calculate whether the skills are available
+    /// Future work: mentorship
+    /// </summary>
     private static List<Project> HaveRequiredSkills(List<Project> projects, List<Contributor> contributors) => projects;
-    //Greedy
+
+    /// <summary>
+    /// Greedily grab the project with the highest score
+    /// </summary>
     private static Project PickBestProject(List<Project> feasibleProjects) => feasibleProjects.MaxBy(p => p.Score);
-    private static void UpdateContributors(List<Contributor> contributors) { return; }
+
+    /// <summary>
+    /// Update skill levels when they completed a project at or below their skill level
+    /// </summary>
+    private static void UpdateContributorSkillLevel(List<Contributor> contributors) { return; }
 }
