e9ced2a7e653f4249239bd818c51925bb7712c6a
RonaldKruizinga <ronmatk@gmail.com>
2022-02-25 11:10:44 +0100

    Rough setup of algorithm

--- app/HashCode.cs
+++ app/HashCode.cs
@@ -2,6 +2,35 @@
 {
     public static Output CreateOutput(Input input)
     {
+        //todo: make mutable copies
+        List<Project> projects = input.Projects.ToList();
+        List<Contributor> contributors = input.Contributors.ToList();
+        List<Project> completedProjects = new();
+        while (projects.Any())
+        {
+            List<Project> feasibleProjects = CalculateFeasible(projects, contributors);
+            if (!feasibleProjects.Any())
+            {
+                break;
+            }
+            Project bestProject = PickBestProject(feasibleProjects);
+            projects.Remove(bestProject);
+            UpdateContributors(contributors);
+            completedProjects.Add(bestProject);
+        }
+
         return new Output();
     }
+
+    private static List<Project> CalculateFeasible(List<Project> projects, List<Contributor> contributors)
+    {
+        //todo: projectassignees
+        projects = HaveRequiredSkills(projects, contributors);
+        return projects;
+    }
+
+    private static List<Project> HaveRequiredSkills(List<Project> projects, List<Contributor> contributors) => projects;
+    //Greedy
+    private static Project PickBestProject(List<Project> feasibleProjects) => feasibleProjects.MaxBy(p => p.Score);
+    private static void UpdateContributors(List<Contributor> contributors) { return; }
 }
