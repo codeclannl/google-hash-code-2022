6307251289981d28cd48ef11006efdbf0f460307
Dirk Lemstra <dirk@lemstra.org>
2022-02-25 11:35:31 +0100

    Add score calculation.

---
+++ app/SampleHashCode.cs
@@ -0,0 +1,26 @@
+﻿internal class SampleHashCode
+{
+    public static Output CreateOutput(Input input)
+    {
+        var output = new Output();
+
+        var planning = new ProjectPlanning(input.Projects.First(x => x.Name == "WebServer"));
+        planning.AddContributor(input.Contributors.First(x => x.Name == "Bob"));
+        planning.AddContributor(input.Contributors.First(x => x.Name == "Anna"));
+
+        output.AddProjectPlanning(planning);
+
+        planning = new ProjectPlanning(input.Projects.First(x => x.Name == "Logging"));
+        planning.AddContributor(input.Contributors.First(x => x.Name == "Anna"));
+
+        output.AddProjectPlanning(planning);
+
+        planning = new ProjectPlanning(input.Projects.First(x => x.Name == "WebChat"));
+        planning.AddContributor(input.Contributors.First(x => x.Name == "Maria"));
+        planning.AddContributor(input.Contributors.First(x => x.Name == "Bob"));
+
+        output.AddProjectPlanning(planning);
+
+        return output;
+    }
+}
